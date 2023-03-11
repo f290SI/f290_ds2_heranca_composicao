e9cf8195b79735c671695804474f2fc099f73ee6
Esdras B Silva <bzr4@icloud.com>
2023-03-11 11:47:37 -0300

    main
    - Uso de interfaces na realizacao de cirurgias.

---
+++ src/domain/ContratoProcediementoCirurgico.java
@@ -0,0 +1,5 @@
+package domain;
+
+public interface ContratoProcediementoCirurgico {
+    void executarProcedimentoCirurgico(Funcionario funcionario);
+}
--- src/domain/Pediatra.java
+++ src/domain/Pediatra.java
@@ -3,7 +3,7 @@ package domain;
 import java.time.LocalDate;
 import java.util.Random;
 
-public class Pediatra extends Medico{
+public class Pediatra extends Medico implements ContratoProcediementoCirurgico{
     public Pediatra(String nome, String email, Integer matricula, LocalDate dataAdmissao, Integer crm) {
         super(nome, email, matricula, dataAdmissao, crm);
     }
@@ -13,6 +13,11 @@ public class Pediatra extends Medico{
         System.out.printf("Iniciando o atendimento de %s...\n", funcionario.getNome());
         aferirTemperatuda(funcionario);
         aferirPressao(funcionario);
+    }
+
+    @Override
+    public void executarProcedimentoCirurgico(Funcionario funcionario) {
+        System.out.println("Realizada a cirirgia em "+funcionario.getNome());
     }
 
     @Override
